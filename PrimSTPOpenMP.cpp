2684f7431dbdbafd78c887e53a49f1efbe8542cc
JFcoZC <jose.zeronca@udlap.mx>
2019-02-18 01:03:05 -0600

    Problema al generar la matriz resuelto, asi como escritura de tiempo de ejecucion de acuerdo a numero de procesadores

--- PrimSTPOpenMP.cpp
+++ PrimSTPOpenMP.cpp
@@ -63,7 +63,7 @@ char **readFile(char* filename)
 	return strsInFile;
 	
 }//FIn funcion readFile
-//--------------------------------------------------------------
+//-----------------------------------------------------------------
 int** getGraph()
 {
 	char **filasArchivo;
@@ -140,6 +140,108 @@ int** getGraph()
 	return graph;
 	
 }//Fin funcion get graph
+//-------------------------------------------------------------
+int** generateRandomGraph(int numVertices)
+{
+	int **randomGraph;
+	int randomValue;
+	
+	VERTICES = numVertices;
+	
+	//Asignar espacio de memoria que ocupa cada fila. Ya que cada fila es un arreglo de apuntadores
+	//de enteros el espacio que se reserva en cada fila/renglon = (SizeOfPointerInt*)
+	//.:. Espacio a reservar para todas las fila = NumeroFilas*(SizeOfPointerInt*)
+	randomGraph = (int **)malloc( (numVertices)*sizeof(int*));
+	
+	//Asignar el espacio para cada elemento dentro de cada fila. Cada fila tiene k columnas
+	//.:. Para cada fila j reservar espacio = NumeroColumnas*(SizeOfInt)
+	for(int k = 0; k  < VERTICES; k++)
+	{
+		randomGraph[k] = (int*)malloc(numVertices*sizeof(int));
+		
+	}//Fin for 1
+	
+	//Generar numeros aleatorios del 0 al 20 y llenar con ellos todos los espacios en matriz
+	for(int i = 0; i < numVertices; i++)
+	{
+		for(int j = 0; j < numVertices; j++)
+		{
+			//srand(time(NULL));	//Seed to generate random numbers
+			randomValue = rand() % 20;
+			
+			//Los valores que den 0 se guardan como INFINITO
+			if(randomValue == 0)
+			{
+				randomValue = 9999;
+			}//Fin if
+			
+			//Hacer que los valores de los caminos sean los mismos
+			//en ambas direcciones
+			randomGraph[i][j] = randomValue;
+			randomGraph[j][i] = randomValue; //Transpuesta de matriz
+			
+		}//Fin for 3
+		
+	}//Fin for 2
+	
+	//Asegurarse que la diagonal principal de la matriz es de puros 0s(Los vertices no pueden tener camino asi mismos)
+	for(int l = 0; l < numVertices; l++)
+	{
+		randomGraph[l][l] = 0;
+		
+	}//FIn for 4
+	
+	return randomGraph;
+	
+}//FIn metodo generateRandomGraph
+//--------------------------------------------------------------
+void writeFile(char* filename, char* content)
+{
+	FILE *fp;
+	int error;
+	
+	//Open a text file for append stream to the end
+	//If there is no file, it is created
+	fp = fopen(filename,"a+");
+	
+
+	//Write content
+	error = fputs(content,fp);
+	if( error  == EOF )
+	{
+		printf("Error al escribir en archivo!\n");
+		printf("ERROR: %i = %s\n", error ,strerror(errno));
+		//clearerr(fp);
+		//fflush(stdout);
+		//Llamada recursiva para ver si ya se hace la escritura sin error
+		//writeFile(filename,content);
+		
+	}//Fin if 1
+	
+	//Cerrar archivo
+	fclose(fp);
+	
+}//Fin writeFile
+//----------------------------------------------------
+void printMatrix( int **matrix )
+{
+	for(int i = 0; i < VERTICES; i++)
+	{
+		
+		printf("(%2i) ", i);
+		
+		for(int j = 0; j < VERTICES; j++)
+		{
+			printf("%4i ", matrix[i][j]);
+			
+		}//Fin for 2
+		
+		printf("\n");
+		
+	}//Fin for 1
+	
+	
+}//Fin funcion printMatrix
 //--------------------------------------------------------------
 //Function that returns the number of the vertex with the shortest distance that has
 //not been visited before
@@ -176,17 +278,28 @@ int minimumVertex(int* weights, int* alreadyVisited)
 	
 }//Fin minimumVertex
 //---------------------------------------------------------------------------
-void paralelPrimMST()
+void paralelPrimMST(int NumVertices)
 {
 	//INITIALIZE MATRIX GRAPH AND ALSO VERTICES VARIABLE
 	int **graph;
-	graph = getGraph();
+	//graph = getGraph();
+	graph = generateRandomGraph(NumVertices);
+	
+	//---- Imprimir matriz generada automaticamente ---
+	//printMatrix(graph);
+	//-------------------------------------------------
 	
 	int vertexVisitedFrom[VERTICES];
 	int weights[VERTICES];
 	int numThreads;
 	int alreadyVisitedVertices[VERTICES]; //1=Visited 0 = NOT visited
 	int randomRoot;
+	
+	char procesadores[10];
+	char strtottime[15];
+	double starttime;
+	double endtime;
+	double totaltime;
 
 	//Initialize arrays; saying that any vertice has been visited
 	for(int i = 0; i < VERTICES; i++)
@@ -202,10 +315,13 @@ void paralelPrimMST()
 	weights[randomRoot] = 0;
 	vertexVisitedFrom[randomRoot] = -1; //Vertex 0 visited from 0 because it is the start VERTEX
 	
+	//--------- INICIO ALGORITMO PARALELO ------
+	starttime = omp_get_wtime();
+	
 	for(int j = 0; j < VERTICES-1; j++)
 	{
-		int actualMinVertex = minimumVertex(weights,alreadyVisitedVertices); //Min local solution
-		alreadyVisitedVertices[actualMinVertex] = 1; //Mark as visited
+		int actualMinVertex = minimumVertex(weights,alreadyVisitedVertices); 	//MIN LOCAL SOLUTION
+		alreadyVisitedVertices[actualMinVertex] = 1; //Mark as visited			//MIN LOCAL SOLUTION
 		
 		int k;
 		
@@ -216,15 +332,20 @@ void paralelPrimMST()
 			#pragma omp for
 			for(k = 0; k < VERTICES; k++)
 			{
+				//---------- MIN GLOBAL SOLUTION --------
 				//If actual k verte has not been alreadyvisited by any thread = alreadyVisitedVertices[k]
 				//and there is a valid path to k from actualVertex (weight different from 0) = graph[actualMinVertex][k] = (graph[actualMinVertex][k] != 0)
 				//and the actual weight to vertex k is lower than actual weight to that vertex
 				if( (graph[actualMinVertex][k] && (alreadyVisitedVertices[k] == 0) ) && (graph[actualMinVertex][k] < weights[k]) )
 				{
+					
 					#pragma omp critical
 					{
+						//-------- BROADCAST NEW VERTEX ADED TO MST -------------
 						//actualMinVertex is a new best vertex to go to vertex k
 						vertexVisitedFrom[k] = actualMinVertex; 
+						
+						//-------- LOCAL UPDATE OF WeightsToEachVertex MADE BY EACH PROCESSOR -------------
 						//update weights with the value calculated from actualMinVertex to vertex k in order all the threads know which is the best
 						//weight to go to vertex k
 						weights[k] = graph[actualMinVertex][k];  //d) Add k to Weights if edge to k is a valid edge (Not 0) 
@@ -238,14 +359,33 @@ void paralelPrimMST()
 		}//Fin pragma 1
 		
 	}//Fin for 2
+	//-------- FIN ALGORITMO PARALELO ----------
 	
-	//Imprimir resultado
-	for(int l = 0; l < VERTICES; l++)
+	endtime = omp_get_wtime();
+	totaltime = endtime - starttime;
+	
+	//----- Imprimir resultado ------------
+	/*for(int l = 0; l < VERTICES; l++)
 	{
 		printf("De: %i ir a %i \n", vertexVisitedFrom[l], l);
 		
-	}//Fin for 1
-	printf("FIN!\n");
+	}//Fin for 1*/
+	//---------------------------------------
+	printf("\nRESULTADO PARALLEL VERSION MST en %f segundos: \n",totaltime);
+	
+	//--- Escribir resultados en archivo txt ---
+	//Convertir entero de numero de procesadores a cadena
+	//Convertir int to char itoa(value,string,base)
+	itoa(NUMTHREADS,procesadores,10); //+1 para no usar 0 en calendario y llegar a ultimo dia
+	
+	//Convert double to string
+	sprintf(strtottime,"%f",totaltime);
+	
+	//Append en archivo de resultados
+	writeFile("resultados.txt",procesadores);
+	writeFile("resultados.txt"," ");
+	writeFile("resultados.txt",strtottime);
+	writeFile("resultados.txt","\n");
 	
 }//Fin paralelPrimMST
 
@@ -256,9 +396,10 @@ int main()
 	//Declaracion de variables
 	
 	//Datos de entrada
+	NUMTHREADS = 8; 		//Numero de procesos
 	
 	//Proceso
-	paralelPrimMST();
+	paralelPrimMST(14000);	//Numero de vertices grafo
 	
 	//Datos de salida
 	
